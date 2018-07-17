3d86a1ef451d4063dd6b53dd29c657d89e0bd589
Laurence <laurenceschinina@gmail.com>
2018-07-17 16:44:09 +1000

    Terrain - Batch Optimisation

---
+++ Unity-Games-world-design/Unity Project/Assets/Scrips/Rendering/DistanceDisable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceDisable : MonoBehaviour {
+
+    public Terrain terrain;
+
+	// Use this for initialization
+	void Start () {
+        terrain.treeDistance = 120;
+        terrain.treeBillboardDistance = 10;
+        terrain.treeCrossFadeLength = 7;
+        terrain.Flush();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
