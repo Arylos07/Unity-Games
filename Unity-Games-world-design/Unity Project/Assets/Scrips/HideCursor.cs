a8cbe2dcb242c7adac9aca8dc555ceb450d7df59
Laurence <laurenceschinina@gmail.com>
2018-07-18 14:57:55 +1000

    Fixed Network Issue and Cursor

--- Unity-Games-world-design/Unity Project/Assets/Scrips/HideCursor.cs
+++ Unity-Games-world-design/Unity Project/Assets/Scrips/HideCursor.cs
@@ -2,20 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-public class HideCursor : MonoBehaviour {
-    private bool isShowingCursor;
+using Photon;
+public class HideCursor : Photon.MonoBehaviour {
+    //private bool isShowingCursor;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-	}
+        //Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        if (photonView.isMine)
+        {
+            Cursor.visible = !Cursor.visible;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-            
+        //Check to see if the current photon view is the local players.
+        if (photonView.isMine)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Cursor.visible = !Cursor.visible;
+                if (!Cursor.visible)
+                {
+                    Cursor.lockState = CursorLockMode.Locked;
+                }
+                else
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                }
+            }
+        }
+
     }
 }
